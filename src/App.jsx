7a166973dcdb15341990cd99f9d4a107ebd58f7f
Patrik <Patrik.Granstrom@metropolia.fi>
2025-04-24 17:47:39 +0300

    Add calendar as a react component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,72 @@
 import './App.css';
+import React, {Component} from 'react';
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
 
 import './App.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+class App extends Component {
+  state = {
+    events: [
+      {
+        start: moment().toDate(),
+        end: moment().add(1, 'days').toDate(),
+        title: 'Some title',
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Calendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={this.state.events}
+          style={{height: '100vh'}}
+        />
+      </div>
+    );
+  }
+}
+
+/*
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/sass/styles';
+
+const localizer = momentLocalizer(moment); // or globalizeLocalizer
+
 const App = () => {
   return (
-    <>
-      <h1>My App</h1>
-    </>
+    <div className="myCustomHeight">
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+      />
+    </div>
   );
 };
+
+// Setup the localizer by providing the moment (or globalize, or Luxon) Object
+// to the correct localizer.
+const localizer = momentLocalizer(moment); // or globalizeLocalizer
+
+const MyCalendar = (props) => (
+  <div className="myCustomHeight">
+    <Calendar
+      localizer={localizer}
+      events={myEventsList}
+      startAccessor="start"
+      endAccessor="end"
+    />
+  </div>
+);*/
+
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+/*import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,4 +7,18 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+)*/
+
+import React from 'react';
+import './index.css';
+import App from './App';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import './index.css';
+//import registerServiceWorker from './registerServiceWorker';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+); //registerServiceWorker();
