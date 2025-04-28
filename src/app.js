a1bef6d16c854555911887cc907d0ea18ab84895
TaruNipuli <taru.nipuli@metropolia.fi>
2025-04-28 14:26:13 +0300

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
-import express from 'express';
-import api from './api/index.js';
+import express from "express";
+import cors from "cors";
+import api from "./api/index.js";
+
 const app = express();
+
+// Enable CORS
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1', api);
+// API routes
+app.use("/api/v1", api);
 
-app.get('/', (req, res) => {
-    res.send('Welcome to our restaurant!')
+// Default route
+app.get("/", (req, res) => {
+  res.send("Welcome to our restaurant");
 });
 
 export default app;
