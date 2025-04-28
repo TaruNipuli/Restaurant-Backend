bca7a549b3d180556308d1907084ee4ad8634f36
CrimsonBambi <gerli.hirv@metropolia.fi>
2025-04-28 20:23:52 +0300

    imported cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import api from './api/index.js';
+import cors from 'cors';
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
